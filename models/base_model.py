ec2f47fa608b8b57b93bef0cf937a23d5fc9f34d
DMMutua <lengowriter99@gmail.com>
2022-11-27 12:38:22 +0300

    Updating uuid importation

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 "Definations and implementation of the BaseModel class"
 
-from uuid import uuid4
+import uuid
 from datetime import datetime
 
 timeformat = "%Y-%m-%dT%H:%M:%S.%f"
@@ -75,3 +75,10 @@ class BaseModel:
 
         return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
+my_model = BaseModel()
+my_model.name = "my first model"
+my_model.my_number = 89
+print(my_model)
+my_model.save()
+print(my_model)
+my_model_json = my_model.to_dict()
