ed2a8b7ee277490f5d08773ec733671b07151b9e
DMMutua <lengowriter99@gmail.com>
2023-01-02 11:48:32 +0300

    linking `BaseModel` to `FileStorage` using the initialized variable `storage`

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 
 import uuid
 from datetime import datetime
+import models
 
 timeformat = "%Y-%m-%dT%H:%M:%S.%f"
 
@@ -39,11 +40,13 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.utcnow()
             self.updated_at = self.created_at
+            models.storage.new(self)
 
     def save(self):
         """Updates the attribute `updated_at` of an object to the current date-time"""
 
         self.updated_at = datetime.utcnow()
+        models.storage.save()
 
     def to_dict(self):
         """Returning the attribute dictionary of key/values  of a particular
