60d2336b7a0e046dedd4124ca64165f8a034bbb4
DMMutua <lengowriter99@gmail.com>
2023-01-02 11:31:39 +0300

    Ensuring the application is always initiated with a unique `FileStorage` instance

---
+++ models/__init__.py
@@ -0,0 +1,14 @@
+"""
+Initialization of a variable `storage`. 
+This Variable creates a unique instance of `FileStorage`
+    for the Application.
+
+Consequently, the `__objects` class attribute of the `FileStorage`
+    class is loaded with all objects currently on the `__file_path`
+    class attribute of the `FileStorage` class.
+"""
+
+from models.engine.file_storage import FileStorage
+
+storage = FileStorage()
+storage.reload()
