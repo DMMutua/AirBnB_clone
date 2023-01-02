6ee2c959f16fc64d7805b842d8ed315b2c0385d9
DMMutua <lengowriter99@gmail.com>
2023-01-02 12:42:19 +0300

    creating console and first navigation methods of CLI

--- console.py
+++ console.py
@@ -1,6 +1,38 @@
 #!/usr/bin/python3
-"""Console for The Airbnb_Clone Project """
+"""
+This File Hosts the Source Code for the entry Point of the Command Line Interpretor.
+This Module Can Create, modify, and delete instances.
+"""
 
-import cmd
-import models
-from models.amenity import amenity
+class HBNBCommand(cmd.Cmd):
+    """
+    The Command Processor Class to be Implemented for the CLI.
+    """
+    # A custom Prompt;
+    prompt = '(hbnb) '
+
+    def do_quit(self, line):
+        """Quit Command; Exiting the Program
+        """
+        return True
+
+    def do_EOF(self, line):
+        """
+        Quit Command: Exiting the Program
+        """
+        return True
+
+    def emptyline(self):
+        """
+        Ensures that the CLI instances of class `HBNBCommand`
+        return nothing when an empty line character is given 
+        as response to prompt. 
+        Overrides default `emptyline` behavior of repeating last
+        nonempty command entered.
+        """
+        pass
+
+
+#Ensuring the program implemented in this file is executable except when Imported.
+if__name__ == '__main__':
+    HBNBCommand().cmdloop()
