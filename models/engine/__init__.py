f15143fcb5786ab14f0e287e1842e78784806d97
DMMutua <lengowriter99@gmail.com>
2023-01-02 11:36:14 +0300

    clearing the engine initialization file

--- models/engine/__init__.py
+++ models/engine/__init__.py
@@ -1,9 +1,1 @@
-#!/usr/bin/python3
-"""Initializing a Unique fileStorage Instance on Importation of the Package
-Uses the __init__ magic method.
-"""
-from models.engine.file_storage import FileStorage
 
-
-storage = FileStorage()
-storage.reload()
