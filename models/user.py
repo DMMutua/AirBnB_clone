82649c517a83c592707fea09b44e530673c6b2c0
DMMutua <lengowriter99@gmail.com>
2023-01-09 10:58:54 +0300

    Adding some public class attributes to user

--- models/user.py
+++ models/user.py
@@ -5,9 +5,15 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """Representation of User """
+    
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
 
     def __init__(self, *args, **kwargs):
         """Initializing a User Object.
         Variable Arguments
         """
         super().__init__(self, *args, **kwargs)
+
