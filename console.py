83ba2c733c05d327d279e9416e102cf61e64c3a2
DMMutua <lengowriter99@gmail.com>
2023-02-08 10:53:50 +0300

    making basemodel.py pycodestyle compliant

--- console.py
+++ console.py
@@ -14,6 +14,7 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
+import shlex
 
 
 class HBNBCommand(cmd.Cmd):
@@ -75,8 +76,9 @@ class HBNBCommand(cmd.Cmd):
             Line < **argv >: A line with the Arguments of an Allowed Class and
                 Valid ID.
         """
-        command = self.parseline(line)[0]
-        arg = self.parseline(line)[1]
+        arg_vector = shlex.split(line)
+        command = arg_vector[0]
+        arg = arg_vector[1]
         if command is None:
             print('** class name missing **')
         elif command not in self.list_of_classes:
@@ -98,22 +100,21 @@ class HBNBCommand(cmd.Cmd):
             Line < **argv >: A line with the Arguments of an Allowed Class and
                 Valid ID.
         """
-        command = self.parseline(line)[0]
-        arg = self.parseline(line)[1]
-        if command is None:
-            print('** class name missing **')
-        elif command not in self.list_of_classes:
-            print("** class doesn't exist **")
-        elif arg == '':
-            print('** instance id missing **')
-        else:
-            instance_key = command + '.' + arg
-            instance_data = models.storage.all().get(instance_key)
-            if inst_data is None:
-                print('** no instance found **')
+        args = shlex.split(line)
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] in self.list_of_classes:
+            if len(args) > 1:
+                key = args[0] + "." + args[1]
+                if key in models.storage.all():
+                    models.storage.all().pop(key)
+                    models.storage.save()
+                else:
+                    print("** no instance found **")
             else:
-                del models.storage.all()[instance_key]
-                models.storage.save()
+                print("** instance id missing **")
+        else:
+            print("** class doesn't exist **")
 
     def do_all(self, line):
         """
@@ -134,6 +135,53 @@ class HBNBCommand(cmd.Cmd):
                     print(str(objects[any_key]))
         else:
             print("** class doesn't exist **")
+
+    def do_update(self, line):
+        """
+        Updating an Instance Based on the class Name and `id`
+        by Adding or Updating an Attribute.
+        Args:
+            line < **argv >: <class name> <id> <attribute name> "<attribute value>"
+        """
+        classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
+
+        args = shlex.split(line)
+        integers = ["number_rooms", "number_bathrooms", "max_guest",
+                    "price_by_night"]
+        floats = ["latitude", "longitude"]
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] in classes:
+            if len(args) > 1:
+                k = args[0] + "." + args[1]
+                if k in models.storage.all():
+                    if len(args) > 2:
+                        if len(args) > 3:
+                            if args[0] == "Place":
+                                if args[2] in integers:
+                                    try:
+                                        args[3] = int(args[3])
+                                    except:
+                                        args[3] = 0
+                                elif args[2] in floats:
+                                    try:
+                                        args[3] = float(args[3])
+                                    except:
+                                        args[3] = 0.0
+                            setattr(models.storage.all()[k], args[2], args[3])
+                            models.storage.all()[k].save()
+                        else:
+                            print("** value missing **")
+                    else:
+                        print("** attribute name missing **")
+                else:
+                    print("** no instance found **")
+            else:
+                print("** instance id missing **")
+        else:
+            print("** class doesn't exist **")
+
 
 #Ensuring the program implemented in this file is executable except when Imported.
 if __name__ == '__main__':
--- models/base_model.py
+++ models/base_model.py
@@ -18,10 +18,10 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Initialization method of objects of the BaseModel Class.
             args:
-                *args: any attributes of the BaseModel Class, 
+                *args: any attributes of the BaseModel Class,
                             allowing dynamic attribute setting
-                **kwargs: key/value pairs for attribute setting on instantiation
-                            of class objects
+                **kwargs: key/value pairs for attribute setting
+                            on instantiation of class objects
         """
 
         if kwargs:
@@ -29,11 +29,14 @@ class BaseModel:
                 if key != "__class__":
                     setattr(self, key, value)
             if kwargs.get("created_at", None) and type(self.created_at) is str:
-                self.created_at = datetime.strptime(kwargs["created_at"], timeformat)
+                self.created_at = datetime.strptime(
+                        kwargs["created_at"], timeformat)
             else:
                 self.created_at = datetime.utcnow()
-            if kwargs.get("updated_at", None) and type(self.updated_at) is str:
-                self.created_at = datetime.strptime(kwargs["created_at"], timeformat)
+            if kwargs.get("updated_at", None) and type(
+                    self.updated_at) is str:
+                self.created_at = datetime.strptime(
+                        kwargs["created_at"], timeformat)
             else:
                 self.created_at = datetime.utcnow()
         else:
@@ -44,42 +47,48 @@ class BaseModel:
             storage.new(self)
 
     def save(self):
-        """Updates the attribute `updated_at` of an object to the current date-time"""
+        """Updates the attribute `updated_at`
+        of an object to the current date-time"""
 
         from models import storage
         self.updated_at = datetime.utcnow()
         storage.save()
 
-    def to_dict(self, save_fs = None):
+    def to_dict(self, save_fs=None):
         """Returning the attribute dictionary of key/values  of a particular
         instance of the BaseClass.
-            the Attributes `created_at` and `updated_at` are returned as type `str`
-            and in ISO format.
+            the Attributes `created_at` and `updated_at`
+            are returned as type `str`and in ISO format.
         """
         new_dict = self.__dict__.copy()
         if "created_at" in new_dict:
             if isinstance(new_dict["created_at"], datetime):
-                new_dict["created_at"] = new_dict["created_at"].strftime("%Y-%m-%dT%H:%M:%S.%f")  # use strftime() to format the time
+                new_dict["created_at"] = new_dict["created_at"].strftime(
+                        "%Y-%m-%dT%H:%M:%S.%f")
+                # use strftime() to format the time above;
         if "updated_at" in new_dict:
             if isinstance(new_dict["updated_at"], datetime):
-                new_dict["updated_at"] = new_dict["updated_at"].strftime("%Y-%m-%dT%H:%M:%S.%f")  # use strftime() to format the time
-        
+                new_dict["updated_at"] = new_dict["updated_at"].strftime(
+                        "%Y-%m-%dT%H:%M:%S.%f")
+                # use strftime() to format the time above;
+
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
         if save_fs is None:
             if "password" in new_dict:
                 del new_dict["password"]
-        
-        return new_dict
 
+        return new_dict
 
     def __str__(self):
         """Returning the string representation of the objects of BaseClass.
             Returns in the format `[<class name>] (<self.id>) <self.__dict__>`.
         """
 
-        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{:s}] ({:s}) {}".format(
+                self.__class__.__name__,
+                self.id, self.__dict__)
 
     def delete(self):
         """Deleting the current Instance from Storage"""
