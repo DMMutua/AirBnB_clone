89f1d4c84ceb854e51e42d8cae0fbcb7d501228e
DMMutua <lengowriter99@gmail.com>
2023-02-24 10:51:05 +0300

    adding public class attribute `name` as empty string

--- models/amenity.py
+++ models/amenity.py
@@ -6,6 +6,8 @@ from models.base_model import BaseModel
 class Amenity(BaseModel):
     """Representation of Amenity """
 
+    name = ""
+
     def __init__(self, *args, **kwargs):
         """Initializing an Amenity Object.
         Variable Arguments
